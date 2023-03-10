9e02a4c67432bf76d482b1c8663c904b7a041bf2
Koushik Dey <koushik.dey@live.com>
2023-03-10 02:11:32 +0200

    Dockerfile for production version

--- client/src/Fib.js
+++ client/src/Fib.js
@@ -5,7 +5,7 @@ class Fib extends Component {
   state = {
     seenIndexes: [],
     values: {},
-    index: ''
+    index: '',
   };
 
   componentDidMount() {
@@ -21,22 +21,22 @@ class Fib extends Component {
   async fetchIndexes() {
     const seenIndexes = await axios.get('/api/values/all');
     this.setState({
-      seenIndexes: seenIndexes.data
+      seenIndexes: seenIndexes.data,
     });
   }
 
-  handleSubmit = async (e) => {
-    e.preventDefault();
+  handleSubmit = async (event) => {
+    event.preventDefault();
 
     await axios.post('/api/values', {
-      index: this.state.index
+      index: this.state.index,
     });
     this.setState({ index: '' });
   };
 
   renderSeenIndexes() {
     return this.state.seenIndexes.map(({ number }) => number).join(', ');
-  };
+  }
 
   renderValues() {
     const entries = [];
@@ -48,7 +48,9 @@ class Fib extends Component {
         </div>
       );
     }
-  };
+
+    return entries;
+  }
 
   render() {
     return (
@@ -57,10 +59,11 @@ class Fib extends Component {
           <label>Enter your index:</label>
           <input
             value={this.state.index}
-            onChange= {e => this.setState({ index: e.target.value})}
+            onChange={(event) => this.setState({ index: event.target.value })}
           />
           <button>Submit</button>
         </form>
+
         <h3>Indexes I have seen:</h3>
         {this.renderSeenIndexes()}
 
@@ -72,5 +75,6 @@ class Fib extends Component {
 }
 
 export default Fib;
+
 
 
