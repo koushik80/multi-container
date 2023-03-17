34e322701c5be2c0100c2e5888f95b1e62cbe150
Koushik Dey <koushik.dey@live.com>
2023-03-17 17:53:06 +0200

    finalized deploy.sh, configured  Gcloud CLI, cloud console, installed helm, ingress-nginx

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,15 @@
-docker build -t koushik80/multi-client -f ./client/Dockerfile ./client
-docker build -t koushik80/multi-server -f ./server/Dockerfile ./server
-docker build -t koushik80/multi-worker -f ./worker/Dockerfile ./worker
-docker push koushik80/multi-client
-docker push koushik80/multi-server
-docker push koushik80/multi-worker
+docker build -t koushik80/multi-client:latest -t koushik80/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t koushik80/multi-server:latest -t koushik80/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t koushik80/multi-worker:latest -t koushik80/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+docker push koushik80/multi-client:latest
+docker push koushik80/multi-server:latest
+docker push koushik80/multi-worker:latest
+
+docker push koushik80/multi-client:$SHA
+docker push koushik80/multi-server:$SHA
+docker push koushik80/multi-worker:$SHA
+
 kubectl apply -f k8s
-kubectl set image deployments/server-deployment server=koushik80/multi-server
+kubectl set image deployments/server-deployment server=koushik80/multi-server:$SHA
+kubectl set image deployments/client-deployment client=koushik80/multi-client:$SHA
+kubectl set image deployments/worker-deployment worker=koushik80/multi-worker:$SHA
